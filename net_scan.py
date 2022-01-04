b4084ec7257c09872cf5e677a881870c1f1701f1
Doomdon <2andrusha3@gmail.com>
2022-01-04 20:02:49 +0300

    add dict and func get_arguments

--- net_scan.py
+++ net_scan.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
 
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")# установка широковещатеьный мак-адрес
     arp_broadcast_request = broadcast/arp_request #объединяем первые два пакета в один
-
     # Ответы записываются в две переменные в виде списков. Нам нужна первая переменная - answ
     answ = scapy.srp(arp_broadcast_request, timeout=1, verbose=False)[0]   #ожидаем ответ в течение 1 секунды
 
     # Сам ответ тож состоит из списка (запрос, ответ). Поэтому мы пробегаем по answ, берем второй элемент(ответ)
     # и выводим из него ip и MAC
+    clients_list = []
+    for el in answ:
+        client_dict = {"ip": el[1].psrc, "mac": el[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+
+def print_result(results_list):
     print("IP\t\t\tMAC Address")
     print("-----------------------------------------------")
-    for el in answ:
-        print(el[1].psrc + "\t\t" + el[1].hwsrc)    #ip и MAC
-        print('-----------------------------------------------')
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
 
-scan("ip")
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
